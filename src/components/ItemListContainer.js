27362965cc261e75eb1025cd9896a5e72d3eccf7
Horacio Demaria <horaciodemaria@gmail.com>
2021-03-06 15:21:09 -0300

    Texto agregado

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,17 +1,17 @@
-import logo from '../logo.svg'
+import tienda from '../tienda.png'
 function Container(){
     return(
         <div class="toast bg-primary text-white" role="alert" aria-live="assertive" aria-atomic="true">
   <div class="toast-header">
-    <img src="" class="rounded mr-2" alt="..." />
-    <strong class="mr-auto">Bootstrap</strong>
-    <small>11 mins ago</small>
+    <img src={tienda} width ="120px" height="25px" class="rounded mr-2" alt="..." />
+    <strong class="mr-auto">Hola, gracias por tu visita</strong>
+    <small> Comencemos </small>
     <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
   </div>
   <div class="toast-body">
-    Hello, world! This is a toast message.
+  Bienvenido a la tienda de ecommerce.Podrás elegir entre muchos productos
   </div>
 </div>
     )
